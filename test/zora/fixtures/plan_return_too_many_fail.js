75bc6a3c44beaafc0372fe13bd9b02fa283d57a6
nichoth <nichoth@nichoth.com>
2025-01-01 21:43:17 -0800

    add failing test

---
+++ test/zora/fixtures/plan_return_too_many_fail.js
@@ -0,0 +1,11 @@
+import { test } from '../../../index.js'
+
+test('Return .plan, execute too many tests', t => {
+    t.ok('test 1')
+    t.ok('test 2')
+    t.ok('test 2')
+    t.ok('test 2')
+    t.ok('test 2')
+
+    return t.plan(3)
+})
