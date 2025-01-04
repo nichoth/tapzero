edec583df788014d67468e34c1770796bf6e963b
nichoth <nichoth@nichoth.com>
2025-01-03 20:08:27 -0800

    wip

---
+++ test/zora/fixtures/plan_return_async.js
@@ -0,0 +1,32 @@
+import { test } from '../../../index.js'
+
+const start = Number(new Date())
+
+test('Return .plan from an async function', async t => {
+    t.ok(true)
+    await sleep(100)
+    t.ok(true)
+    await sleep(100)
+    t.ok(true)
+
+    return t.plan(3, 2000)
+})
+
+test('check the times', t => {
+    const finish = Number(new Date())
+    const diff = finish - start
+    console.log('**finish**', finish - start)
+    t.ok(diff >= 200, 'should wait for a little bit')
+    t.ok(diff < 1000, 'should resolve the test before it times out')
+})
+
+/**
+ * Wait for a number of miliseconds.
+ * @param {number} ms 
+ * @returns {Promise<void>}
+ */
+async function sleep (ms) {
+    await new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
